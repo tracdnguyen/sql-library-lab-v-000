bae5c282f0e72da63277f03c0b31e1d68bebf2c7
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-06 20:10:43 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -42,5 +42,10 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name
+   FROM characters
+   LEFT JOIN character_books
+   ON characters.id = character_books.character_id
+   GROUP BY characters.name
+   ORDER BY COUNT(characters.book_id)"
 end
