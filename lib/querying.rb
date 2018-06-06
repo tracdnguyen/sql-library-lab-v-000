2955a422e9d3fa8f974eed9022c030da3ec20ce8
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-06 20:08:59 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,7 +36,7 @@ def select_series_title_with_most_human_characters
    LEFT JOIN characters
    ON series.id = characters.series_id
    GROUP BY series.title, characters.species
-   HAVING character.species = 'human'
+   HAVING characters.species = 'human'
    ORDER BY COUNT(characters.species) DESC
    LIMIT 1"
 end
