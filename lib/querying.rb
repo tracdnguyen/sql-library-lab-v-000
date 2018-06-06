cf3fda652e309c4aeaac81aa5e8edcfd8cc0b30f
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-06 20:08:46 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,7 +27,7 @@ def select_name_and_series_subgenres_of_authors
    JOIN series
    ON authors.id = series.author_id
    JOIN subgenres
-   ON series.subgenres_id = subgenres.id"
+   ON series.subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
