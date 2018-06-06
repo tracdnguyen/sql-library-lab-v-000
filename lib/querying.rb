fadf7aa85136e1483e23017d251e11c15185fc7e
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-06 20:11:53 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -42,7 +42,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name
+  "SELECT characters.name, COUNT(character_books.book_id)
    FROM characters
    LEFT JOIN character_books
    ON characters.id = character_books.character_id
